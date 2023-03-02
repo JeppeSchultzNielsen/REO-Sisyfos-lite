1687ee86971fc675a3c09d03d8163900bbdaf9b9
Jeppe Schultz Nielsen <jeppe-schultz@hotmail.com>
2023-03-02 19:34:06 +0100

    Made outageplans for 25, 30, 35

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from Simulation import Simulation
 from DataHolder import DataHolder
 from Options import Options
 
-dh = DataHolder(2040,2009,"data/outage/Plan2040.csv")
+dh = DataHolder(2030,2009,"data/outage/Plan2030.csv")
 options = Options()
 options.usePlannedDownTime = True
 options.useUnplannedDownTime = False
 
 
-sim = Simulation(options, dh, 2040,2009,"results/NewDemand.txt", True)
+sim = Simulation(options, dh, 2030,2009,"results/NewDemand30.txt", True)
+sim.RunSimulation(0,10)
+
+dh = DataHolder(2025,2009,"data/outage/Plan2025.csv")
+options = Options()
+options.usePlannedDownTime = True
+options.useUnplannedDownTime = False
+
+
+sim = Simulation(options, dh, 2025,2009,"results/NewDemand25.txt", True)
 sim.RunSimulation(0,10)
