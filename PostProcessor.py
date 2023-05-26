07d15f595fa1ec7c5b8d0f28dc1ae6d2d56ffa25
Jeppe Schultz Nielsen <jeppe-schultz@hotmail.com>
2023-05-26 13:08:31 +0200

    Change to main

--- PostProcessor.py
+++ PostProcessor.py
@@ -62,7 +62,7 @@ class PostProcessor:
                              "HydroPumpOL",
                              "HydroPumpCL",
                              "Battery",
-                             "DSR"]#this list must contain all types givein in the outageParams file
+                             "DSR"]#this list must contain all types given in the outageParams file
         self.names = ["DK1", "DK2", "DKBO", "DKKF", "DKEI", "NOn","NOm","NOs","SE1","SE2","SE3","SE4","FI","DELU","AT","NL","GB","FR","BE","ESPT","CH","IT","EELVLT", "PL", "CZSK","HU"]
 
     def process(self, toProcess: str, writeTo: str):
--- main.py
+++ main.py
@@ -12,9 +12,11 @@ options.useVariations = True
 options.tyndpYear = 2030
 options.prioritizeVariableProduction = True
 dh = DataHolder(options,"data/outage/Plan2035_1985_var.csv")
-sim = Simulation(options, dh,f"results/test2.txt", True, False)
-sim.RunSimulation(0,100)
-pp = PostProcessor()
-pp.process(f"results/test2.txt","results/test2pp.txt")
 
-#climateYears = [1985,1987,1996,2007,2008,2009,2010,2011,2012,2013,2014,2015]
+for i in range(2):
+    sim = Simulation(options, dh,f"results/2035_1985_{i}.txt", True, False)
+    sim.RunSimulation(0,8760)
+    pp = PostProcessor()
+    pp.process(f"results/2035_1985_{i}.txt",f"results/2035_1985_{i}_pp.txt")
+
+#climateYears = [1985,1987,1996,20På 07,2008,2009,2010,2011,2012,2013,2014,2015]
